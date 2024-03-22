2896d0860a33612531770366009fd33f9d9b308a
daydayup001001 <2523436332@qq.com>
2024-03-22 17:29:13 +0800

    发行说明

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
-	<div style="height: 100%">
-		<split-pane :min-percent="0" :default-percent="20" split="vertical" class-name="ggggg">
-			<template v-slot:paneL> vertical-A </template>
-			<template v-slot:paneR>
-				<split-pane split="horizontal" :default-percent="75">
-					<template v-slot:paneL> horizontal-B </template>
-					<template v-slot:paneR>
-						<split-pane split="vertical" :default-percent="75">
-							<template v-slot:paneL> vertical-C </template>
-							<template v-slot:paneR> vertical-D </template>
-						</split-pane>
-					</template>
-				</split-pane>
-			</template>
-		</split-pane>
+	<div class="main">
+		<div class="demo-A">
+			<split-pane :min-percent="0" :default-percent="20" split="vertical" class-name="ggggg">
+				<template v-slot:paneL> vertical-A </template>
+				<template v-slot:paneR>
+					<split-pane split="horizontal" :default-percent="75">
+						<template v-slot:paneL> horizontal-B </template>
+						<template v-slot:paneR>
+							<split-pane split="vertical" :default-percent="75">
+								<template v-slot:paneL> vertical-C </template>
+								<template v-slot:paneR> vertical-D </template>
+							</split-pane>
+						</template>
+					</split-pane>
+				</template>
+			</split-pane>
+		</div>
+		<div class="demo-B">
+			<split-pane :min-percent="0" :default-percent="20" split="vertical" class-name="split-A">
+				<template v-slot:paneL> vertical-A </template>
+				<template v-slot:paneR>
+					<split-pane split="vertical" :default-percent="40">
+						<template v-slot:paneL> vertical-B </template>
+						<template v-slot:paneR>
+							<split-pane split="vertical" :default-percent="40">
+								<template v-slot:paneL :default-percent="20"> vertical-C </template>
+								<template v-slot:paneR> vertical-D </template>
+							</split-pane>
+						</template>
+					</split-pane>
+				</template>
+			</split-pane>
+		</div>
 	</div>
 </template>
 
@@ -22,4 +40,21 @@ import { defineAsyncComponent } from "vue";
 const SplitPane = defineAsyncComponent(() => import("@/components/SplitPane/index.vue"));
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.main {
+	height: 100%;
+	padding: 20px;
+
+	.demo-A {
+		height: 400px;
+		margin-bottom: 50px;
+		border: 1px solid black;
+	}
+
+	.demo-B {
+		width: 80%;
+		height: 600px;
+		border: 1px solid black;
+	}
+}
+</style>
